67a9c5a23ce6fcb1169bc1725811e9b6113b770f
robson <robson.ronzani@palmas.to.gov.br>
2018-08-31 20:31:13 -0300

    Static

--- crud_nosql/settings.py
+++ crud_nosql/settings.py
@@ -14,7 +14,7 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-LOCAL = os.path.dirname(os.path.abspath(__file__))
+# LOCAL = os.path.dirname(os.path.abspath(__file__))
 
 
 # Quick-start development settings - unsuitable for production
@@ -128,5 +128,5 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(LOCAL, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
